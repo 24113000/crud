dc07f94aeffd80bcd567e4070c5f47dd178d682e
Sergii Bezgin <sergii.bezgin@abakus.me>
2015-12-03 19:43:19 +0200

    configured maven

---
+++ src/main/java/com/sbezgin/crud/SandBoxTester.java
@@ -0,0 +1,11 @@
+package com.sbezgin.crud;
+
+/**
+ * Created by sergii.bezgin on 03.12.2015.
+ * It is my class ha-ha-ha
+ */
+public class SandBoxTester {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
