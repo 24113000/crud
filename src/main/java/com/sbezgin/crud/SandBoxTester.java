6b10b04c9b9ec16452e0a0d60c99a9c60fd96b54
Sergii Bezgin <sergii.bezgin@abakus.me>
2015-12-05 01:21:52 +0200

    configured hibernate

--- src/main/java/com/sbezgin/crud/SandBoxTester.java
+++ src/main/java/com/sbezgin/crud/SandBoxTester.java
@@ -1,11 +1,30 @@
 package com.sbezgin.crud;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.model.naming.ImplicitNamingStrategyJpaCompliantImpl;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
 /**
  * Created by sergii.bezgin on 03.12.2015.
  * It is my class ha-ha-ha
  */
 public class SandBoxTester {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        StandardServiceRegistry standardRegistry = new StandardServiceRegistryBuilder()
+                .configure("hibernateConf.xml")
+                .build();
+
+        Metadata metadata = new MetadataSources(standardRegistry)
+                .addPackage("com.sbezgin.db")
+                .getMetadataBuilder()
+                .applyImplicitNamingStrategy(ImplicitNamingStrategyJpaCompliantImpl.INSTANCE)
+                .build();
+
+        SessionFactory sessionFactory = metadata.getSessionFactoryBuilder().build();
+        Session session = sessionFactory.openSession();
     }
 }
---
+++ src/main/java/com/sbezgin/db/User.java
@@ -0,0 +1,55 @@
+package com.sbezgin.db;
+
+/**
+ * Created by sergii.bezgin on 05.12.2015.
+ * It is my class ha-ha-ha
+ */
+public class User {
+    private int id;
+    private String userName;
+    private String email;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (email != null ? !email.equals(user.email) : user.email != null) return false;
+        if (userName != null ? !userName.equals(user.userName) : user.userName != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = userName != null ? userName.hashCode() : 0;
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        return result;
+    }
+}
